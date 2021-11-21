b81cd4bf830dd2689943dd80937c4b083e32b211
Mohith N <mohithshivu03@gmail.com>
2021-11-21 20:34:23 +0530

    Test Initial Commit 3

--- src/FirstJavaProgram.java
+++ src/FirstJavaProgram.java
@@ -1,5 +1,35 @@
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("Hi Mohith N");
+import java.sql.*;
+import java.lang.*;
+public class FirstJavaProgram
+{
+    public static void main(String[] args) throws Exception
+    {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/demodatabase";
+        String uname = "root";
+        String  password = "Mohith";
+        Connection con = DriverManager.getConnection(url,uname,password);
+        System.out.println("Test: "+con.getCatalog());
+
+
+        String query1 ="select * from data1";
+        String query ="delete from data1 where name ='RamRaj'";
+        Statement st = con.createStatement();
+        st.executeUpdate(query);
+        ResultSet res = st.executeQuery(query1);
+        ResultSetMetaData resmd = res.getMetaData();
+        int colcount = resmd.getColumnCount();
+        while(res.next())
+        {
+            for(int i=1;i<=colcount;i++)
+            {
+                System.out.println(res.getString(i)+" ");
+            }
+            //System.out.println();
+        }
+        //DBTablePrinter.printTable(con,"demodatabase");
+        con.close();
+
+
     }
 }
